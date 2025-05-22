8506eedccf5ff347bf8b47a6de844043858e2d20
dajung-y <0352wjd@naver.com>
2025-05-22 21:40:35 +0900

    Day9 제출 - 윤다정

---
+++ Day9/dajung/js/ifEx.js
@@ -0,0 +1,61 @@
+// 1번 문제
+
+let num=9;
+if(9%2 ===0){
+  console.log('짝수입니다.');
+} else{
+  console.log('홀수입니다.');
+}     
+
+// 2번 문제
+
+let score=83;
+
+if(score>=90){
+  console.log('A 학점입니다.');
+} else if(score>=80){
+  console.log('B 학점입니다.');
+} else if(score>=70){
+  console.log('C 학점입니다.');
+} else if(score>=60){
+  console.log('D 학점입니다.');
+} else {
+  console.log('F학점입니다.');
+}
+
+// 3번 문제
+
+let age=4;
+
+if(0<=age<=3){
+  console.log('무료입니다.');
+} else if(4<=age<=12){
+  console.log('입장료는 5,000원입니다.');
+} else if(13<=age<=64){
+  console.log('입장료는 10,000원입니다.')
+} else {
+  console.log('입장료는 7,000원입니다.')
+}
+
+// 4번 문제
+
+let isLoggedIn=true;
+
+if(isLoggedIn===true){
+  console.log('환영합니다!');
+} else{
+  console.log('로그인해주세요.');
+}
+
+// 5번 문제
+
+let a=10;
+let b=25;
+
+if(a>b){
+  console.log('a가 더 큽니다.');
+} else if(a<b){
+  console.log('b가 더 큽니다');
+} else{
+  console.log('두 수는 같습니다.');
+}
